f8c1f7f0a87027257423ebd6258204a4e5c1f26c
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-01-12 11:57:03 +0200

    Add files via upload

---
+++ DataStructuresAdvanced/05. Hash-Tables-Sets-and-Dictionaries-Lab-Skeleton/HashTable/DuplicateKeyException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public class DuplicateKeyException : ArgumentException
+    {
+        public DuplicateKeyException(string message, string paramName) 
+            : base(message, paramName)
+        {
+
+        }
+    }
+}
