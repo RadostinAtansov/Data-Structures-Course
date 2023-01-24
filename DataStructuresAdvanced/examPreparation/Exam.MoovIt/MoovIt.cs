0094c1616bf43d83f63278e9211d6969961e0a13
Radostin Atanasov <16541785+RadostinAtansov@users.noreply.github.com>
2023-01-24 22:25:31 +0200

    Add files via upload

---
+++ DataStructuresAdvanced/examPreparation/Exam.MoovIt/MoovIt.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam.MoovIt
+{
+    public class MoovIt : IMoovIt
+    {
+        HashSet<Route> routes = new HashSet<Route>();
+
+        public int Count => this.routes.Count;
+
+        public void AddRoute(Route route)
+        {
+            if (this.routes.Contains(route))
+            {
+                throw new ArgumentException();
+            }
+            this.routes.Add(route);
+        }
+
+        public void ChooseRoute(string routeId)
+        {
+            var route = this.routes.Where(r => r.Id == routeId).FirstOrDefault(); // might be slow
+            if (route == null)
+            {
+                throw new ArgumentException();
+            }
+            route.Popularity++;
+
+        }
+
+        public bool Contains(Route route)
+        {
+            return this.routes.Contains(route);
+        }
+
+        public IEnumerable<Route> GetFavoriteRoutes(string destinationPoint)
+        {
+            return routes.Where(r => r.IsFavorite && r.LocationPoints.IndexOf(destinationPoint) >= 1)
+                .OrderBy(r => r.Distance)
+                .ThenByDescending(r => r.Popularity);
+        }
+
+        public Route GetRoute(string routeId)
+        {
+            var route = this.routes.Where(r => r.Id == routeId).FirstOrDefault(); // might be slow
+            if (route == null)
+            {
+                throw new ArgumentException();
+            }
+            return route;
+        }
+
+        public IEnumerable<Route> GetTop5RoutesByPopularityThenByDistanceThenByCountOfLocationPoints()
+        {
+            return this.routes
+                .OrderByDescending(r => r.Popularity)
+                .ThenBy(r => r.Distance)
+                .ThenBy(r => r.LocationPoints.Count)
+                .Take(5);
+        }
+
+        public void RemoveRoute(string routeId)
+        {
+            var route = this.routes.Where(r => r.Id == routeId).FirstOrDefault(); // might be slow
+            if (route == null)
+            {
+                throw new ArgumentException();
+            }
+
+            this.routes.Remove(route);
+        }
+
+        public IEnumerable<Route> SearchRoutes(string startPoint, string endPoint)
+        {
+            return this.routes
+            .Where(r => r.LocationPoints.Contains(startPoint)
+                     && r.LocationPoints.Contains(endPoint)
+                     && r.LocationPoints.IndexOf(startPoint) < r.LocationPoints.IndexOf(endPoint))
+            .OrderBy(r => r.IsFavorite)
+            .ThenBy(r => r.LocationPoints.IndexOf(endPoint) - r.LocationPoints.IndexOf(startPoint))
+            .ThenByDescending(r => r.Popularity);
+        }
+    }
+}
---
+++ DataStructuresAdvanced/examPreparation/Exam.MoovIt/Route.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Exam.MoovIt
+{
+    public class Route
+    {
+        public string Id { get; set; }
+
+        public double Distance { get; set; }
+
+        public int Popularity { get; set; }
+
+        public bool IsFavorite { get; set; }
+
+        public List<string> LocationPoints { get; set; } = new List<string>();
+
+        public Route(string id, double distance, int popularity, bool isFavorite, List<string> locationPoints)
+        {
+            this.Id = id;
+            this.Distance = distance;
+            this.Popularity = popularity;
+            this.IsFavorite = isFavorite;
+            this.LocationPoints = locationPoints;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Route;
+
+            return this.Distance == other.Distance
+                && this.LocationPoints[0] == other.LocationPoints[0]
+                && this.LocationPoints[this.LocationPoints.Count - 1]
+                == other.LocationPoints[other.LocationPoints.Count - 1];
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Distance.GetHashCode() 
+                * this.LocationPoints[0].GetHashCode()
+                * this.LocationPoints[this.LocationPoints.Count - 1].GetHashCode();
+        }
+    }
+} 
